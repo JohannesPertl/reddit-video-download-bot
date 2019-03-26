d10c4ee9d6bb744f06f99a4f5f0d0973c085bc3f
Synapsensalat <synapsensalat@protonmail.com>
2019-03-26 22:15:16 +0100

    Update bot.py
    Restructuring

--- bot.py
+++ bot.py
@@ -1,93 +1,154 @@
+# !/usr/bin/env python3
 
-#!/usr/bin/env python3
-#############################################################################
-## Reddit Bot that provides downloadable links for v.redd.it videos (Audio)##
-#############################################################################
+# Reddit Bot that provides downloadable links for v.redd.it videos (Audio)
+
 
 import praw
 import time
 import re
-import sys
 import random
 import os.path
 import os
-import smtplib
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import urllib.request, urllib.error
 import certifi
 from urllib.request import Request, urlopen
-from pyvirtualdisplay import Display
 import youtube_dl
 import pomf
 import requests
 from praw.models import Comment
 from praw.models import Message
-import sqlite3
-display = Display(visible=0, size=(800, 600))
-display.start()
 
 
-#GLOBAL VARIABLES
-npSubs = 'furry_irl', 'pcmasterrace'
-#Default path
-botPath = '/home/pi/bots/vreddit/'
-format = '.mp4'
-blacklistSubs = ['The_Donald']
-blacklistUsers = ['cyXie', 'airboy1021', 'null']
+# Constants
+BOT_NAME = "u/vreddit_bot"
+DATA_PATH = '/home/pi/bots/vreddit/data/'
+VIDEO_FORMAT = '.mp4'
+COMMENTED_PATH = DATA_PATH + 'commented.txt'
+BLACKLIST_USERS = ['null', 'AutoModerator']
+ANNOUNCEMENT = "\n\nUse your mobile browser if your app has problems opening my links."
+FOOTER = ("  \n ***  \n ^I\'m&#32;a&#32;Bot&#32;*bleep*&#32;*bloop*&#32;|&#32;[**Contact&#32;Developer**](https://np.reddit.com/message/compose?to=/u/Synapsensalat)&#32;|&#32;[**Info**](https://np.reddit.com/r/vredditDownloader/comments/9h41sx/info)&#32;|&#32;[**Donate**](https://np.reddit.com/r/vredditDownloader/wiki/index)")
+INBOX_LIMIT = 25
+RATELIMIT = 2000000
+MAX_FILESIZE = int('200000000')
 
-#Upload on catbox.moe or mixtape.moe
-def uploadPomf(filePath, site):
+
+def main():
+    reddit = authenticate()
+    while True:
+        # Search mentions in inbox
+        inbox = list(reddit.inbox.unread(limit=INBOX_LIMIT))
+        inbox.reverse()
+        for item in inbox:
+            author = str(item.author)
+
+            # Check requirements
+            match_type = type_of_item(item)
+            if not match_type:
+                continue
+            elif match_type == "comment":
+                submission = item.submission
+            else:
+                submission = get_real_reddit_submission(reddit, match_type)
+
+            if "v.redd.it" not in submission.url:
+                continue
+
+            # Get media and audio URL
+            media_url = create_media_url(submission, reddit)
+            if not media_url:
+                media_url = submission.url
+                reply_no_audio = ""
+            else:
+                reply_no_audio = '* [**Soundless video**](' + media_url + ')'
+
+            audio_url = media_url.rpartition('/')[0] + '/audio'
+            reply = reply_no_audio
+            header = "#Downloadable link:\n\n"
+            
+            if media_url == submission.url or has_audio(audio_url):
+                if media_url == submission.url:
+                    reply_audio_only = ""
+                else:
+                    reply_audio_only = '* [**Audio only**](' + audio_url + ')'
+
+                download_path = DATA_PATH + 'downloaded/' + str(submission.id) + VIDEO_FORMAT
+                upload_path = DATA_PATH + 'uploaded/' + str(submission.id) + '.txt'
+
+                # Upload
+                uploaded_url = upload(media_url, submission.url, download_path, upload_path)
+                if uploaded_url:
+                    # Create log file with uploaded link, named after the submission ID
+                    create_uploaded_log(upload_path, uploaded_url)
+                    try:
+                        reply_audio = "* [**Video with sound**](" + uploaded_url + ")"
+                        reply = reply_audio + '\n\n' + reply_no_audio + '\n\n' + reply_audio_only
+                        header = "#Downloadable links:\n\n"
+                    except Exception as e:
+                        print(e)
+
+            reply = header + reply + ANNOUNCEMENT
+
+            reply_to_user(item, reply, reddit, author)
+
+        time.sleep(2)
+
+
+def upload_pomf(file_path, site):
     if site == 'catbox':
         files = {
             'reqtype': (None, 'fileupload'),
-            'fileToUpload': (filePath, open(filePath, 'rb')),
+            'fileToUpload': (file_path, open(file_path, 'rb')),
         }
-
         response = requests.post('https://catbox.moe/user/api.php', files=files)
         return response.text
 
     elif site == 'mixtape':
         host = pomf.get_host('mixtape')
-        ret = host.upload(open(filePath, 'rb'))
+        ret = host.upload(open(file_path, 'rb'))
         return ret['url']
-        # {'hash': 'f6bb5ef07fe63759ecfac1c81193c5912b96c45b', 'name': 'hearts.png', 'url': 'https://my.mixtape.moe/hsoali.png', 'size': 25553}
 
 
-def download(downloadedPath,submissionURL):
-        try:
-            ydl_opts = {
-            'outtmpl': downloadedPath,
-            #'format': 'bestvideo',        #uncomment for video without audio only, see youtube-dl documentation
-            'max_filesize': int('200000000'),
-            'ratelimit': 2000000,
-            }
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([submissionURL])
-            return True
+def download(download_url, download_path):
 
-        except Exception as e:
-            print('ERROR: Downloading failed.')
-            print(e)
-            return False
+    try:
+        ydl_opts = {
+            'outtmpl': download_path,
+            'max_filesize': MAX_FILESIZE,
+            'ratelimit': RATELIMIT,
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([download_url])
+        return download_path
 
-
-#Authenticate via praw.ini file, look at praw documentation for more info
+    except Exception as e:
+        print('ERROR: Downloading failed.')
+        print(e)
+        return ""
+    
+    
+# Authenticate via praw.ini file, look at praw documentation for more info
 def authenticate():
     print('Authenticating...\n')
-    reddit = praw.Reddit('vreddit', user_agent = 'vreddit_bot')
+    reddit = praw.Reddit('vreddit', user_agent='vreddit')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
 
-# Upload Video via vreddi.it
-def uploadVreddit(url):
 
-    driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+# Upload Video via vredd.it
+def upload_via_vreddit(url):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=options)
     webpage_url = 'https://vredd.it'
     driver.get(webpage_url)
 
-    urlBox = driver.find_element_by_id('r_url')
+    url_box = driver.find_element_by_id('r_url')
 
-    urlBox.send_keys(url)
+    url_box.send_keys(url)
 
     login_button = driver.find_element_by_id('submit_url')
     login_button.click()
@@ -100,205 +161,163 @@ def uploadVreddit(url):
         except:
             counter += 1
             continue
-    uploadedURL = driver.find_element_by_class_name('btn').get_attribute('href')
+    uploaded_url = driver.find_element_by_class_name('btn').get_attribute('href')
     driver.quit()
-    return uploadedURL
+    return uploaded_url
 
 
-#############
-## RUN BOT ##
-#############
+def check_url_valid(url):
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        urllib.request.urlopen(req, cafile=certifi.where())
+        return True
+    except:
+        return False
 
-def run_bot(reddit):
-    #Search mentions in inbox
-    inbox = list(reddit.inbox.unread(limit = 10))
-    inbox.reverse()
-    for item in inbox:
-        s = ''
-        matchLink = False
-        body = str(item.body)
-        author = str(item.author)
-        matchText = re.search(r"(?i)u/vreddit_bot", body)
-        footer = ("  \n ***  \n ^^I\'m&#32;a&#32;Bot&#32;*bleep*&#32;*bloop*&#32;|&#32;[**Contact**](https://np.reddit.com/message/compose?to=/u/Synapsensalat)&#32;|&#32;[**Info**](https://np.reddit.com/r/vreddit_bot/comments/9h41sx/info)&#32;|&#32;[**Donate**](https://np.reddit.com/r/vreddit_bot/wiki/index)")
 
-        if isinstance(item, Comment) and matchText:
-            mention = item
-            s = item.submission
-            if str(mention.subreddit) in npSubs:
-                footer = ''
-        elif isinstance(item, Message):
-            message = item
-            matchLink = re.search(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", body)
+def has_audio(url):
+    # Workaround to check if v.redd.it link has audio
+    try:
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        resp.read()
+        return True
+    except:
+        return False
 
-        reply = ''
-        vredditSite = False
-        uploadedURL = ''
-        replyWithAudio = False
-        createTxtFile = False
 
+def create_uploaded_log(upload_path, uploaded_url):
+    try:
+        print('Creating txt file.')
+        f = open(upload_path, "w+")
+        f.write(uploaded_url)
+        f.close()
+    except Exception as e:
+        print(e)
+        print("ERROR: Can't create txt file.")
+
+
+def reply_to_user(item, reply, reddit, user):
+    print('Replying... \n')
+    try:
+        item.reply(reply + FOOTER)
+        item.mark_read()
+
+    # Send PM if replying went wrong (Should only happen if the bot is banned)
+    except Exception as e:
+        print(e)
         try:
-            if matchLink:
-                link = re.sub('DASH.*', '', matchLink[0])
-                s = reddit.submission(url=requests.get(link).url)
-
-            submissionURL = s.url
-            submissionID = s.id
-            blacklisted = s.subreddit in blacklistSubs or author in blacklistUsers
-
-            matchVreddit = 'v.redd.it' in submissionURL
-        except Exception as e:
-            matchVreddit = False
-
-
-        if matchVreddit and (matchText or matchLink) and not blacklisted:
-            #Check if the file was downloaded or uploaded before
-            downloadedPath = botPath + 'downloaded/' + str(submissionID) + format
-            uploadedPath = botPath + 'uploaded/' + str(submissionID) + '.txt'
-            downloadedFileExists = os.path.exists(downloadedPath)
-            uploadedLogExists = os.path.exists(uploadedPath)
-
-            #Check if file has already been uploaded (if the .txt file with the link was generated before, to avoid uploading the same video multiple times)
-            if uploadedLogExists:
-                try:
-                    with open(uploadedPath, 'r') as content_file:
-                        uploadedURL = content_file.read()
-                        uploadedTxt = True
-
-                except Exception as e:
-                    print(e)
-                    print("Couldn't get URL, continuing..")
-            else:
-                uploadedTxt = False
-
-            #Generate downloadable v.redd.it URL
-            try:
-                mediaURL = s.media['reddit_video']['fallback_url']
-                mediaURL = str(mediaURL)
-                audioURL = mediaURL.rpartition('/')[0] + '/audio'
-                # vreddit = True
-            except Exception as e:
-                try:
-                    crosspostID = s.crosspost_parent.split('_')[1]
-                    s = reddit.submission(crosspostID)
-                    mediaURL = s.media['reddit_video']['fallback_url']
-                    mediaURL = str(mediaURL)
-                    audioURL = mediaURL.rpartition('/')[0] + '/audio'
-                except Exception as f:
-                    continue
-
-             #Workaround to check if v.redd.it link has audio
-            try:
-                req = urllib.request.Request(audioURL)
-                resp = urllib.request.urlopen(req)
-                respData = resp.read()
-                hasAudio= True
-            except:
-                hasAudio= False
-
-            if not uploadedTxt and hasAudio:
-                try:
-                    uploadedURL = uploadVreddit(mediaURL)
-
-                    req = Request(uploadedURL, headers={'User-Agent': 'Mozilla/5.0'})
-                    try:
-                        conn = urllib.request.urlopen(req, cafile=certifi.where())
-                    except urllib.error.HTTPError as e:
-                        # Return code error (e.g. 404, 501, ...)
-                        # ...
-                        print('HTTPError: {}'.format(e.code))
-                    except urllib.error.URLError as e:
-                        # Not an HTTP-specific error (e.g. connection refused)
-                        # ...
-                        print('URLError: {}'.format(e.reason))
-                    else:
-                        # 200
-                        vredditSite = True
-                        createTxtFile = True
-                        replyWithAudio = True
-
-                except Exception as e:
-                    print("Couldn't upload: ")
-                    vredditSite = False
-                    print(e)
-
-                if not vredditSite:
-                    print("vredd.it didn't work, downloading..")
-                    downloaded = download(downloadedPath, submissionURL)
-                    if downloaded:
-                        try:
-                            uploadedURL = uploadPomf(downloadedPath, 'catbox')
-                            replyWithAudio = True
-                            createTxtFile = True
-                            os.remove(downloadedPath)
-                        except:
-                            try:
-                                uploadedURL = uploadPomf(downloadedPath, 'mixtape')
-                                replyWithAudio = True
-                                createTxtFile = True
-                                os.remove(downloadedPath)
-                            except:
-                                replyWithAudio = False
-
-                    else:
-                        replyWithAudio = False
-
-            elif not hasAudio:
-                replyWithAudio = False
-            else:
-                replyWithAudio = True
-
-
-            replyAudio = "* [**Video with sound**]("+ uploadedURL + ")"
-            replyNoAudio = '* [**GIF**](' + mediaURL + ')'
-            replyAudioOnly = '* [**Audio only**](' + audioURL + ')'
-
-            if replyWithAudio:
-                reply = replyAudio + '\n\n' + replyNoAudio + '\n\n' + replyAudioOnly
-            else:
-                reply = replyNoAudio
-
-            announcement = "\n\nUse your mobile browser if your app has problems opening my links."
-            if matchText:
-                announcementPM = "\n\nIf you don't want to mention my name you can also send me a private message (chat messages don't work, sorry) containing the link."
-                announcement = random.choice([announcement,announcementPM])
-            reply = "#Downloadable links:\n\n" + reply + announcement
-
-            print('Replying... \n')
-            try:
-                item.reply(reply + footer)
-
-            #Send PM if replying went wrong (Should only happen if the bot is banned)
-            except:
-                sub = str(mention.subreddit)
-                pm = reply + footer
-                subject = 'I\'m probably banned in r/' + sub + ', so you get a PM instead ;)'
-                print('Can\'t comment, probably banned. Replying per PM.')
-                reddit.redditor(author).message(subject, pm)
-
-
+            pm = reply + FOOTER
+            subject = "I couldn't reply to your comment so you get a PM instead :)"
+            print('Can\'t comment, probably banned. Replying per PM.')
+            reddit.redditor(user).message(subject, pm)
             item.mark_read()
-
-            #Create .txt file with uploaded link, named after the submission ID
-            if createTxtFile:
-                try:
-                    print('Creating txt file.')
-                    f = open(uploadedPath,"w+")
-                    f.write(uploadedURL)
-                    f.close()
-                except Exception as e:
-                    print(e)
-                    print("ERROR: Can't create txt file.")
+        except Exception as f:
+            print(f)
 
 
-    time.sleep(2)
+def is_url_valid(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        urllib.request.urlopen(req, cafile=certifi.where())
+    except Exception as e:
+        return False
+    else:
+        return True
 
 
-def main():
-    reddit = authenticate()
-    while True:
-        run_bot(reddit)
+def create_media_url(submission, reddit):
+    media_url = "False"
+    try:
+        media_url = submission.media['reddit_video']['fallback_url']
+        media_url = str(media_url)
+    except Exception as e:
+        print(e)
+        try:
+            crosspost_id = submission.crosspost_parent.split('_')[1]
+            s = reddit.submission(crosspost_id)
+            media_url = s.media['reddit_video']['fallback_url']
+        except Exception as f:
+            print(f)
+            print("Can't read media_url, skipping")
 
+    return media_url
+
+
+def get_real_reddit_submission(reddit, url):
+    link = re.sub('DASH.*', '', url)
+    s = reddit.submission(url=requests.get(link).url)
+    return s
+
+
+def type_of_item(item):
+    body = str(item.body)
+    match_text = re.search(r"(?i)" + BOT_NAME, body)
+    match_link = re.search(
+        r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)", body)
+
+    if isinstance(item, Comment) and match_text:
+        return "comment"
+
+    elif isinstance(item, Message) and match_link:
+        return match_link[0]
+
+    return ""
+
+
+def uploaded_log_exists(upload_path):
+
+    if not os.path.exists(upload_path):
+        return ""
+
+    try:
+        with open(upload_path, 'r') as content_file:
+            uploaded_url = content_file.read()
+            if not is_url_valid(uploaded_url):
+                print("Old URL not valid anymore, deleting..")
+                os.remove(upload_path)
+                return ""
+            return uploaded_url
+    except Exception as e:
+        print(e)
+        print("Couldn't get URL, continuing..")
+        return ""
+
+
+def upload(media_url, download_url, download_path, upload_path):
+    # Check if already uploaded before
+    uploaded_url = uploaded_log_exists(upload_path)
+    if uploaded_url:
+        return uploaded_url
+
+    try:
+        uploaded_url = upload_via_vreddit(media_url)
+        # Sometimes vredd.it returns invalid url
+        if is_url_valid(uploaded_url):
+            return uploaded_url
+
+    except Exception as e:
+        print(e)
+
+    print("Couldn't upload to https://vredd.it, downloading..")
+    try:
+        download_path = download(download_url, download_path)
+    except:
+        return ""
+
+    try:
+        uploaded_url = upload_pomf(download_path, 'catbox')
+    except:
+        try:
+            uploaded_url = upload_pomf(download_path, 'mixtape')
+        except Exception as e:
+            print(e)
+            print("Couldn't upload")
+            return ""
+
+    os.remove(download_path)
+    return uploaded_url
 
 
 if __name__ == '__main__':
-        main()
+    main()
